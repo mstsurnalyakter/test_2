9b5a418c5f8d40079c6b97d5766c74e2551990c5
mstsurnalyakter <surnalyms@gmail.com>
2024-11-07 22:08:33 +0600

    Footer add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <NavBar/>
       <h1>Hello</h1>
       <h2>Surnaly</h2>
+      <Footer />
 
     </div>
   )
