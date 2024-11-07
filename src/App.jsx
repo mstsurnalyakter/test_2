67121a9e8c9bb7de3cc91b7a7d0968fd9db26ea9
mstsurnalyakter <surnalyms@gmail.com>
2024-11-07 22:06:33 +0600

    add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
+      <NavBar/>
       <h1>Hello</h1>
       <h2>Surnaly</h2>
 
-    </>
+    </div>
   )
 }
 
